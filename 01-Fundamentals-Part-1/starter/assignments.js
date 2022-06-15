b6fb6d0b3944b41905718bddcbda11be35200b63
Tsvetan Petrov <ts.petrov@draftkings.com>
2022-06-15 17:55:36 +0300

    code challenges 2,3 and 4

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -24,18 +24,58 @@ print();
 splitConutryPopulation();
 increasePopulationWithOne();
 
-//Coding Challenge #1
-let MarksWeight = 78;
-let MarksHeight = 1.69
-let JohnsWeight = 92;
-let JohnsHeight = 1.95
+//Coding Challenge 1 and 2
+const MarksWeight = 78;
+const MarksHeight = 1.69
+const JohnsWeight = 92;
+const JohnsHeight = 1.95;
 
 function calculateBMI(height, mass) {
     return BMI = mass / height ** 2
 }
 
 let markHigherBMI = false;
-if (calculateBMI(MarksHeight, MarksWeight) > calculateBMI(JohnsHeight, JohnsWeight)) {
+let marksBMI = calculateBMI(MarksHeight, MarksWeight);
+let johnsBMI = calculateBMI(JohnsHeight, JohnsWeight);
+if (marksBMI > johnsBMI) {
     markHigherBMI = true;
 }
-console.log(markHigherBMI)
+
+if (markHigherBMI) {
+    console.log(`Mark\'s BMI (${marksBMI.toFixed(2)}}) is higher than John\'s (${johnsBMI.toFixed(2)})!`);
+}
+else {
+    console.log(`John's BMI (${johnsBMI}) is higher than Mark's (${marksBMI})!`);
+}
+
+//Codding challenge 3
+const firstTeamResults = [96, 108, 89];
+const secondTeamResults = [88, 91, 110];
+
+let avgFirstTeam = (firstTeamResults.reduce(function (accumVariable, curValue) {
+    return accumVariable + curValue
+}, 0)) / firstTeamResults.length;
+
+let avgSecontTeam = (secondTeamResults.reduce(function (accumVariable, currValue) {
+    return accumVariable + currValue
+}, 0)) / secondTeamResults.length;
+if (avgFirstTeam > avgSecontTeam && avgFirstTeam >= 100) {
+    console.log(`Winner is the first team with avg score: ${avgFirstTeam.toFixed(2)}`);
+} else if (avgFirstTeam < avgSecontTeam && avgSecontTeam >= 100) {
+    console.log(`Winner is the second team with avg score: ${avgSecontTeam.toFixed(2)}`);
+} else if (avgFirstTeam >= 100) {
+    console.log(`Draw, both teams have ${avgFirstTeam.toFixed(2)} avg score`);
+} else {
+    console.log(`No team wins, both teams have ${avgFirstTeam.toFixed(2)} avg score, which is not enough`);
+}
+
+//Coding Challenge 4
+
+const bills = [275, 40, 430]
+
+bills.forEach(bill => {
+    let isBetweenRange = (bill >= 50 || bill <= 300) ? true : false;
+    let tip = isBetweenRange ? 0.15 * bill : 0.20 * bill;
+
+    console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
+});
