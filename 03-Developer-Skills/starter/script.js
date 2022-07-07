20cf0bf2478290520359b4010f3f0383033f9295
Tsvetan Petrov <ts.petrov@draftkings.com>
2022-07-07 17:09:19 +0300

    guess my number work

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
+const arr = [17, 21, 23];
+const arr2 = [12, 5, -5, 0, 4];
+const printForecast = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`${arr[i]} in ${i + 1} days`);
+  }
+};
 
+printForecast(arr);
+printForecast(arr2);
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,46 @@
 'use strict';
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+let msg = 'Start guessing...';
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    msg = 'No number!';
+  } else if (guess === secretNumber) {
+    checkScore();
+    msg = 'Correct!';
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      msg = 'Too big!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      msg = 'Game Over!';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      msg = 'Too small!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      msg = 'Game Over!';
+    }
+  }
+  displayMsg(msg);
+});
+const displayMsg = function (msg) {
+  document.querySelector('.message').textContent = msg;
+};
+const checkScore = function () {
+  if (score > highScore) {
+    highScore = score;
+    score = 20;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.highscore').textContent = highScore;
+  }
+};
