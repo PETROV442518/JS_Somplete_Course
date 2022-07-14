f5c74fb30a8dfc2e61ecbc93126c8d7aa8a45f7b
Tsvetan Petrov <ts.petrov@draftkings.com>
2022-07-14 16:52:45 +0300

    refactor Guess my Number app

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -3,6 +3,10 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 let msg = 'Start guessing...';
+const greenBackground = '#60b347';
+const blackBackground = '#222';
+const numberFieldWiderSize = '30rem';
+const numberFieldNormalSize = '15rem';
 
 const displayMsg = function (msg) {
   document.querySelector('.message').textContent = msg;
@@ -17,34 +21,34 @@ const checkScore = function () {
 
 const updateScore = function () {
   score--;
+  if (score < 1) {
+    score = 0;
+    msg = 'Game Over!';
+    displayMsg(msg);
+  }
   document.querySelector('.score').textContent = score;
+};
+
+const correctResultOutput = function () {
+  document.querySelector('body').style.backgroundColor = greenBackground;
+  document.querySelector('.number').style.width = NumberFieldWiderSize;
+  document.querySelector('.number').textContent = secretNumber;
 };
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
   if (!guess) {
     msg = 'No number!';
   } else if (guess === secretNumber) {
     checkScore();
     msg = 'Correct!';
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.number').textContent = secretNumber;
+    correctResultOutput();
   } else if (guess > secretNumber) {
-    if (score > 1) {
-      msg = 'Too big!';
-      updateScore();
-    } else {
-      msg = 'Game Over!';
-    }
+    msg = 'Too big!';
+    updateScore();
   } else if (guess < secretNumber) {
-    if (score > 1) {
-      msg = 'Too small!';
-      updateScore();
-    } else {
-      msg = 'Game Over!';
-    }
+    msg = 'Too small!';
+    updateScore();
   }
   displayMsg(msg);
 });
@@ -52,8 +56,8 @@ document.querySelector('.check').addEventListener('click', function () {
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   document.querySelector('.score').textContent = score;
-  document.querySelector('.number').style.width = '15rem';
-  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = numberFieldNormalSize;
+  document.querySelector('body').style.backgroundColor = blackBackground;
   msg = 'Start guessing...';
   document.querySelector('.number').textContent = '?';
   secretNumber = Math.trunc(Math.random() * 20) + 1;
